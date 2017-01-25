58a68523b9bad4ab05a635697f49fd0ecaf32114
Brandon Ekins <ekinsb@byu.local>
2017-01-24 18:29:18 -0700

    updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(3000, function() {
+connect().use(serveStatic(__dirname + '/public')).listen(3000, function() {
 	console.log('server running on port 3000');
 })
